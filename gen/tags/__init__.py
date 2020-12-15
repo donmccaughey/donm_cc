5a8254c3a647c883555c05d5052a341af97a98fd
Don McCaughey <don@donm.cc>
2020-12-14 20:29:59 -0800

    Renamed `tags.tags` package to `tags.elements`.

--- gen/tags/__init__.py
+++ gen/tags/__init__.py
@@ -1,6 +1,6 @@
 from .doctype import DocType
 from .document import Document
 from .text import Text
-from .tags import A, Body, Br, Button, Code, Div, Em, Form, H1, H2, Head, HTML
-from .tags import Img, Input, Label, Li, MetaCharset, MetaViewport, Nav, P
-from .tags import Script, Section, Span, Strong, Stylesheet, Time, Title, Ul
+from .elements import A, Body, Br, Button, Code, Div, Em, Form, H1, H2, Head, HTML
+from .elements import Img, Input, Label, Li, MetaCharset, MetaViewport, Nav, P
+from .elements import Script, Section, Span, Strong, Stylesheet, Time, Title, Ul
