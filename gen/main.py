662d4276a4d1c893f940ad9e9129a3a357c355e9
Don McCaughey <don@donm.cc>
2021-01-02 22:42:40 -0800

    Added Bash page.

--- gen/main.py
+++ gen/main.py
@@ -143,6 +143,15 @@ with root:
             book('The Adventures of Johnny Bunko', 'https://www.amazon.com/Adventures-Johnny-Bunko-Career-Guide-ebook/dp/B0015DRPL8', checked=True)
             book("What Got You Here Won't Get You There", 'https://www.amazon.com/What-Here-There-illustrated-version-ebook/dp/B00710YFJY', checked=True)
 
+    with IndexPage('Bash'):
+        with Section(class_names=['overview']):
+            H1('Bash: the "Bourne-again" shell')
+            P()
+        with Section(class_names=['links']):
+            link('blog', 'Use the Unofficial Bash Strict Mode (Unless You Looove Debugging)', 'http://redsymbol.net/articles/unofficial-bash-strict-mode/', authors='Aaron Maxwell')
+            link('blog', 'How to write idempotent Bash scripts', 'https://arslan.io/2019/07/03/how-to-write-idempotent-bash-scripts/', authors='Fatih Arslan', date='2019-07-03')
+            link('blog', 'Shortcuts to Move Faster in Bash Command Line', 'http://teohm.github.io/blog/2012/01/04/shortcuts-to-move-faster-in-bash-command-line/', authors='Huiming Teo', date='2012-01-04')
+
     with IndexPage('Engineering Management'):
         with Section(class_names=['overview']):
             H1('Engineering Management')
