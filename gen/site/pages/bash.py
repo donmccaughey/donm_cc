4a8b37d3ef3b7e6b5348bd0dcfc55eddce641271
Don McCaughey <don@donm.cc>
2021-02-16 19:55:22 -0800

    Extracted `bash()` page function.

---
+++ gen/site/pages/bash.py
@@ -0,0 +1,23 @@
+from markup import Section, H1, P, Ul
+from resources import IndexPage
+from site.links import link
+
+
+def bash():
+    with IndexPage('Bash'):
+        with Section(class_names=['overview']):
+            H1('Bash: the "Bourne-again" shell')
+            P()
+        with Section(class_names=['links']):
+            H1('Tips and Tricks')
+            with Ul():
+                link('blog',
+                     'Use the Unofficial Bash Strict Mode (Unless You Looove Debugging)',
+                     'http://redsymbol.net/articles/unofficial-bash-strict-mode/',
+                     authors='Aaron Maxwell')
+                link('blog', 'How to write idempotent Bash scripts',
+                     'https://arslan.io/2019/07/03/how-to-write-idempotent-bash-scripts/',
+                     authors='Fatih Arslan', date='2019-07-03')
+                link('blog', 'Shortcuts to Move Faster in Bash Command Line',
+                     'http://teohm.github.io/blog/2012/01/04/shortcuts-to-move-faster-in-bash-command-line/',
+                     authors='Huiming Teo', date='2012-01-04')
--- gen/site/root.py
+++ gen/site/root.py
@@ -5,6 +5,7 @@ from site.collection import item
 from site.links import book, link
 from site.packages import package
 from site.pages.aughey import aughey
+from site.pages.bash import bash
 from site.pages.business_novels import business_novels
 from site.pages.home import home
 
@@ -12,7 +13,6 @@ from site.pages.home import home
 root = home()
 
 with root:
-
     File('base.css')
     Directory('banners')
     Directory('icons')
@@ -20,17 +20,7 @@ with root:
 
     aughey()
     business_novels()
-
-    with IndexPage('Bash'):
-        with Section(class_names=['overview']):
-            H1('Bash: the "Bourne-again" shell')
-            P()
-        with Section(class_names=['links']):
-            H1('Tips and Tricks')
-            with Ul():
-                link('blog', 'Use the Unofficial Bash Strict Mode (Unless You Looove Debugging)', 'http://redsymbol.net/articles/unofficial-bash-strict-mode/', authors='Aaron Maxwell')
-                link('blog', 'How to write idempotent Bash scripts', 'https://arslan.io/2019/07/03/how-to-write-idempotent-bash-scripts/', authors='Fatih Arslan', date='2019-07-03')
-                link('blog', 'Shortcuts to Move Faster in Bash Command Line', 'http://teohm.github.io/blog/2012/01/04/shortcuts-to-move-faster-in-bash-command-line/', authors='Huiming Teo', date='2012-01-04')
+    bash()
 
     with IndexPage('Engineering Management'):
         with Section(class_names=['overview']):
