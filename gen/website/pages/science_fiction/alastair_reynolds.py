fb7756b0df2ac3676ce216288d321ccef2afa356
Don McCaughey <don@donm.cc>
2021-02-18 21:47:55 -0800

    Created parser for links page text format.
    Added `ParseError` exception class to capture parser errors.
    
    Developed grammer for links page format and created simple recursive
    descent `Parser` class that implements the grammer.  The parser simply
    prints out messages during parsing for now.
    
    TODO: correctly report an error when the parse is incomplete.

--- gen/website/pages/science_fiction/alastair_reynolds.py
+++ gen/website/pages/science_fiction/alastair_reynolds.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum, auto
+from typing import Iterator
 
 from markup import Section, H1, P, Ul, A, Text
 from resources import Page
@@ -150,6 +151,228 @@ def tokenize(source: str) -> Token:
             paragraph.append(line)
     if paragraph:
         yield Token(TokenType.PARAGRAPH, '\n'.join(paragraph))
+
+
+class ParserError(RuntimeError):
+    def __init__(self, token: Token, message: str):
+        super().__init__(message)
+
+
+"""
+grammar:
+    
+    page = declaration 
+         | declaration sections
+    
+    declaration = page_directive 
+                | page_directive paragraphs
+    
+    page_directive = '.page' 'links' DATA
+    
+    paragraphs = PARAGRAPH 
+               | PARAGRAPH paragraphs
+    
+    sections = section 
+             | section sections
+    
+    section = section_directive
+            | section_directive paragraphs
+            | section_directive paragraphs links
+            | section_directive links 
+            
+    section_directive = '.section' 'links' DATA
+    
+    links = link
+          | link links
+    
+    link = link_directive url_directive
+         | link_directive url_directive date_directive
+         | link_directive url_directive date_directive checked_directive
+         | link_directive url_directive checked_directive
+    
+    link_directive = '.link' 'book' DATA
+    
+    url_directive = '.url' DATA
+    
+    date_directive = '.date' DATA
+    
+    checked_directive = '.checked'
+"""
+class Parser:
+    def __init__(self, lexer: Iterator[Token]):
+        self.lexer = lexer
+        self.token = None
+        self.error = None
+
+    def start(self) -> bool:
+        self.next_token()
+        # TODO: detect incomplete parse
+        return self.page()
+
+    def next_token(self):
+        try:
+            self.token = next(self.lexer)
+        except StopIteration:
+            self.token = None
+
+    def is_data(self) -> bool:
+        return (
+            self.token
+            and TokenType.DATA == self.token.type
+        )
+
+    def is_directive(self, directive) -> bool:
+        return (
+            self.token
+            and TokenType.DIRECTIVE == self.token.type
+            and directive == self.token.text
+        )
+
+    def is_modifier(self, modifier) -> bool:
+        return (
+            self.token
+            and TokenType.MODIFIER == self.token.type
+            and modifier == self.token.text
+        )
+
+    def is_paragraph(self) -> bool:
+        return (
+            self.token
+            and TokenType.PARAGRAPH == self.token.type
+        )
+
+    def page(self) -> bool:
+        if not self.declaration():
+            return False
+        self.sections()
+        return True
+
+    def declaration(self) -> bool:
+        if not self.page_directive():
+            return False
+        self.paragraphs()
+        return True
+
+    def page_directive(self) -> bool:
+        if not self.is_directive('page'):
+            self.error = ParserError(self.token, 'Expected .page directive')
+            return False
+        self.next_token()
+        if not self.is_modifier('links'):
+            self.error = ParserError(self.token, 'Expected links modifier')
+            return False
+        self.next_token()
+        if not self.is_data():
+            self.error = ParserError(self.token, 'Expected page title')
+            return False
+        print(f'links page {self.token.text}')
+        self.next_token()
+        return True
+
+    def paragraphs(self) -> bool:
+        if not self.is_paragraph():
+            self.error = ParserError(self.token, 'Expected a paragraph')
+            return False
+        print(f'paragraph "{self.token.text[:20]}..."')
+        self.next_token()
+        self.paragraphs()
+        return True
+
+    def sections(self) -> bool:
+        if not self.section():
+            return False
+        self.sections()
+        return True
+
+    def section(self) -> bool:
+        if not self.section_directive():
+            return False
+        if self.paragraphs():
+            self.links()
+        else:
+            self.links()
+        return True
+
+    def section_directive(self) -> bool:
+        if not self.is_directive('section'):
+            self.error = ParserError(self.token, 'Expected .section directive')
+            return False
+        self.next_token()
+        if not self.is_modifier('links'):
+            self.error = ParserError(self.token, 'Expected links modifier')
+            return False
+        self.next_token()
+        if not self.is_data():
+            self.error = ParserError(self.token, 'Expected section title')
+            return False
+        print(f'links section {self.token.text}')
+        self.next_token()
+        return True
+
+    def links(self) -> bool:
+        if not self.link():
+            return False
+        self.link()
+        return True
+
+    def link(self) -> bool:
+        if not self.link_directive():
+            return False
+        if not self.url_directive():
+            return False
+        if self.date_directive():
+            self.checked_directive()
+        else:
+            self.checked_directive()
+        return True
+
+    def link_directive(self) -> bool:
+        if not self.is_directive('link'):
+            self.error = ParserError(self.token, 'Expected .links directive')
+            return False
+        self.next_token()
+        if not self.is_modifier('book'):
+            self.error = ParserError(self.token, 'Expected book modifier')
+            return False
+        self.next_token()
+        if not self.is_data():
+            self.error = ParserError(self.token, 'Expected link title')
+            return False
+        print(f'book link for {self.token.text}')
+        self.next_token()
+        return True
+
+    def url_directive(self) -> bool:
+        if not self.is_directive('url'):
+            self.error = ParserError(self.token, 'Expected .url directive')
+            return False
+        self.next_token()
+        if not self.is_data():
+            self.error = ParserError(self.token, 'Expected URL')
+            return False
+        print(f'link URL {self.token.text}')
+        self.next_token()
+        return True
+
+    def date_directive(self) -> bool:
+        if not self.is_directive('date'):
+            self.error = ParserError(self.token, 'Expected .date directive')
+            return False
+        self.next_token()
+        if not self.is_data():
+            self.error = ParserError(self.token, 'Expected date')
+            return False
+        print(f'link date {self.token.text}')
+        self.next_token()
+        return True
+
+    def checked_directive(self) -> bool:
+        if not self.is_directive('checked'):
+            self.error = ParserError(self.token, 'Expected .checked directive')
+            return False
+        print('link checked')
+        self.next_token()
+        return True
 
 
 page = LinksPage(
