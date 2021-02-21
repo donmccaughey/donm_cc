6fe70ed1d6b1761fdec55139a00aef1218081b79
Don McCaughey <don@donm.cc>
2021-02-20 21:40:04 -0800

    Extracted `Token` creation helper methods.
    This centeralizes the code for processing the raw token data (removing
    the leading '.' for directives, striping trailing space and unencoding
    for paragraphs, striping space for data).

--- gen/file_formats/links_page/lexer.py
+++ gen/file_formats/links_page/lexer.py
@@ -15,8 +15,25 @@ class Token:
     type: TokenType
     text: str
 
+    @staticmethod
+    def data(data: str) -> 'Token':
+        return Token(TokenType.DATA, data.strip())
 
-modifiers = ['links', 'book']
+    @staticmethod
+    def directive(directive: str) -> 'Token':
+        return Token(TokenType.DIRECTIVE, directive[1:])
+
+    @staticmethod
+    def modifier(modifier: str) -> 'Token':
+        return Token(TokenType.MODIFIER, modifier)
+
+    @staticmethod
+    def paragraph(lines: list[str]) -> 'Token':
+        lines = [ unescape(line.rstrip()) for line in lines ]
+        return Token(TokenType.PARAGRAPH, '\n'.join(lines))
+
+
+MODIFIERS = ['links', 'book']
 
 
 def unescape(text: str) -> str:
@@ -28,23 +45,23 @@ def lexer(source: str) -> Iterator[Token]:
     for line in source.splitlines():
         if 0 == len(line) or line.isspace():
             if paragraph:
-                yield Token(TokenType.PARAGRAPH, '\n'.join(paragraph))
+                yield Token.paragraph(paragraph)
                 paragraph = []
         elif '.' == line[0]:
             if paragraph:
-                yield Token(TokenType.PARAGRAPH, '\n'.join(paragraph))
+                yield Token.paragraph(paragraph)
                 paragraph = []
             directive, *rest_of_line = line.split(None, 1)
-            yield Token(TokenType.DIRECTIVE, directive[1:])
+            yield Token.directive(directive)
             if rest_of_line:
                 modifier, *data = rest_of_line[0].split(None, 1)
-                if modifier in modifiers:
-                    yield Token(TokenType.MODIFIER, modifier)
+                if modifier in MODIFIERS:
+                    yield Token.modifier(modifier)
                     if data:
-                        yield Token(TokenType.DATA, data[0].strip())
+                        yield Token.data(data[0])
                 else:
-                    yield Token(TokenType.DATA, rest_of_line[0].strip())
+                    yield Token.data(rest_of_line[0])
         else:
-            paragraph.append(unescape(line.rstrip()))
+            paragraph.append(line)
     if paragraph:
-        yield Token(TokenType.PARAGRAPH, '\n'.join(paragraph))
+        yield Token.paragraph(paragraph)
