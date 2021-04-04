4d0c78f9907a68e7db49d8e8fc4abd68eab0010b
Don McCaughey <don@donm.cc>
2021-04-04 14:43:49 -0700

    Replace use of `IndexPage` in `memory_match()`.
    Use `Directory` and `Page` instead.

--- gen/website/pages/memory_match.py
+++ gen/website/pages/memory_match.py
@@ -1,11 +1,11 @@
 from markup import Div
-from resources import IndexPage
+from resources import Directory, Page
 
 
 def memory_match():
-    global page
-    with IndexPage('Memory Match', has_files=True) as page:
-        page.add_script(
-            'https://cdnjs.cloudflare.com/ajax/libs/cash/1.3.0/cash.min.js')
-        page.add_script('memory_match.js')
-        Div(id='memory_match')
+    with Directory('memory_match'):
+        with Page('Memory Match', name='index') as page:
+            page.add_script(
+                'https://cdnjs.cloudflare.com/ajax/libs/cash/1.3.0/cash.min.js')
+            page.add_script('memory_match.js')
+            Div(id='memory_match')
