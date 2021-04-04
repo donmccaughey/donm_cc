1a193c6558e9ca4eb82ab7b3101d33c8035c0978
Don McCaughey <don@donm.cc>
2021-04-03 23:53:16 -0700

    Replace the use of `IndexPage` in `business_novels()`.
    Use `Directory` and `Page` instead.

--- gen/website/pages/aughey.py
+++ gen/website/pages/aughey.py
@@ -3,7 +3,7 @@ from resources import Directory, Page
 
 
 def aughey():
-    with Directory('aughey', has_files=True):
+    with Directory('aughey'):
         with Page('Don McCaughey', name='index'):
             with Div(class_names=['banner']):
                 Img(src='/aughey/handstand.jpg', alt='Don doing a handstand')
