3944bc69a7ab79f822a3d82bed03a12a747c3b6a
Don McCaughey <don@donm.cc>
2021-04-10 15:11:32 -0700

    Extract root directory creation from the `home()` function.
    Change `home()` so it produces the home page only and rename it to
    `home_page()`.  This makes creation of `root` more explicit.

---
+++ gen/website/pages/home_page.py
@@ -0,0 +1,40 @@
+from markup import Div, Img, Br, Span, Ul
+from resources import Page
+from website.collection import item
+
+
+def home_page():
+    with Page('Don McCaughey', name='index'):
+        with Div(class_names=['banner']):
+            Img(
+                src='/banners/Don_and_Molly_Round_Hill_Lake_Tahoe_summer_2019.jpg',
+                alt='Don and Molly atop Round Hill'
+            )
+            Br()
+            Span(
+                text='Round Hill, Lake Tahoe, summer 2019',
+                class_names=['lower-caption']
+            )
+        with Ul(class_names=['collection']):
+            item('Sourcehut', 'https://git.sr.ht/~donmcc', 'donmcc', external=True)
+            item('GitHub', 'https://github.com/donmccaughey', 'donmccaughey',
+                 favicon='/icons/github.png', external=True)
+            item('Twitter', 'https://twitter.com/donmccaughey', '@donmccaughey',
+                 favicon='/icons/twitter.png', external=True)
+            item('LinkedIn', 'https://www.linkedin.com/in/donmccaughey',
+                 'donmccaughey', favicon='/icons/linkedin.png', external=True)
+            item('Truework', 'https://www.truework.com', 'My current gig',
+                 favicon='/icons/truework.png', external=True)
+            item('Copper', 'https://www.copper.com', 'My previous gig',
+                 favicon='/icons/copper.png', external=True)
+            item('Résumé', '/resume/Resume_of_Don_McCaughey.pdf', "Stuff I've done")
+            item('Memory Match', '/memory_match/', 'A tile matching game')
+            item('Random Words', '/random_words/', 'A handy word picker')
+            item('macOS Packages', '/macos_packages/', 'Software installers',
+                 favicon='/macos_packages/package-32x32.png')
+            item('Engineering Management', '/engineering_management/',
+                 'Software is a team sport')
+            item('Business Novels', '/business_novels/', 'Fictionalized learning')
+            item('Make', '/make/', 'The build tool')
+            item('Hashtables', '/hashtables/', 'Keys and values')
+            item('Science Fiction', '/science_fiction/', 'Reading for fun')
--- gen/website/root.py
+++ gen/website/root.py
@@ -3,7 +3,7 @@ from website.pages.aughey import aughey
 from website.pages.bash import bash
 from website.pages.engineering_management import engineering_management
 from website.pages.hashtables import hashtables
-from website.pages.home import home
+from website.pages.home_page import home_page
 from website.pages.macos_packages import macos_packages
 from website.pages.make import make
 from website.pages.memory_match import memory_match
@@ -12,8 +12,10 @@ from website.pages.rust_and_wasm import rust_and_wasm
 from website.pages.science_fiction import science_fiction
 
 
-root = home()
+root = Directory(name='', is_root=True)
 with root:
+    home_page()
+
     Directory('banners')
     Directory('business_novels')
     Directory('icons')
