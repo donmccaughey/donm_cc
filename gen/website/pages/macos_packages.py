e4a2006d9bf5da8577cea537cdc1d68e01fdea96
Don McCaughey <don@donm.cc>
2021-10-27 14:04:31 -0700

    Updated the `macos_packages/` page.
    Updated the header to mention that the installers are now notarized and
    include universal binaries.
    
    Added the `nginx` package to the list.
    
    Updated the packages for the latest versions.
    
    Updated the package links for all packages to use GitHub "latest" URLs
    in the form `<project>/releases/latest/download/<pkg file name>`.

--- gen/website/pages/macos_packages.py
+++ gen/website/pages/macos_packages.py
@@ -7,38 +7,47 @@ def macos_packages():
     with Directory('macos_packages'):
         with Page('macOS Packages', name='index'):
             with Section(class_names=['overview']):
-                H1('Signed macOS Installer Packages')
+                H1('Signed and Notarized Universal macOS Installer Packages')
                 with P():
                     Text('These standard macOS installer packages are built using ')
                     Code('pkgbuild')
                     Text(' and ')
                     Code('productbuild')
-                    Text(' and are signed with my Apple developer credentials.')
+                    Text(' and are signed and notarized with my Apple developer credentials.')
+                    Text(' They install universal binaries that will run on both Intel and Apple Silicon Macs.')
                 P("""
                     This is a collection of command line tools that I've found useful
                     at one time or another over the years. Some of them are widely used
                     but excluded from macOS and Xcode due to GPL licenses.            
                 """)
             package(
+                name='nginx',
+                version='1.20.1',
+                package='https://github.com/donmccaughey/nginx_pkg/releases/latest/download/nginx-1.20.1.pkg',
+                source='https://github.com/donmccaughey/nginx_pkg',
+                project='https://nginx.org',
+                description='A widely used and capable HTTP and proxy server'
+            )
+            package(
                 name='pkg-config',
-                version='0.29.1',
-                package='https://github.com/donmccaughey/pkg-config_pkg/releases/download/v0.29.2-r1/pkg-config-0.29.2.pkg',
+                version='0.29.2',
+                package='https://github.com/donmccaughey/pkg-config_pkg/releases/latest/download/pkg-config-0.29.2.pkg',
                 source='https://github.com/donmccaughey/pkg-config_pkg',
                 project='https://www.freedesktop.org/wiki/Software/pkg-config/',
                 description='A helper tool used when compiling applications and libraries.'
             )
             package(
                 name='tree',
-                version='1.7.0',
-                package='https://github.com/donmccaughey/tree_pkg/releases/download/v1.7.0-r1/tree-1.7.0.pkg',
+                version='1.8.0',
+                package='https://github.com/donmccaughey/tree_pkg/releases/latest/download/tree-1.8.0.pkg',
                 source='https://github.com/donmccaughey/tree_pkg',
                 project='http://mama.indstate.edu/users/ice/tree/',
                 description='A recursive directory listing command.'
             )
             package(
                 name='XZ Utils',
-                version='5.2.4',
-                package='https://github.com/donmccaughey/xz_pkg/releases/download/v5.2.4-r1/xz-5.2.4.pkg',
+                version='5.2.5',
+                package='https://github.com/donmccaughey/xz_pkg/releases/latest/download/xz-5.2.5.pkg',
                 source='https://github.com/donmccaughey/xz_pkg',
                 project='https://tukaani.org/xz/',
                 description='A general purpose data compression tool and library, and includes the <code>xz</code> command line tool.'
