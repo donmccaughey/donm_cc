61f815e20df92e161b050339f053a9de9a463ae1
Don McCaughey <don@donm.cc>
2022-01-24 21:40:01 -0800

    Convert the "Make" page to a page file.
    Replace the `make()` function in the `website.pages.make` package with a
    page file in `site_src/make`.

--- gen/website/root.py
+++ gen/website/root.py
@@ -4,7 +4,6 @@ from website.pages.bash import bash
 from website.pages.hashtables import hashtables
 from website.pages.home_page import home_page
 from website.pages.macos_packages import macos_packages
-from website.pages.make import make
 from website.pages.memory_match import memory_match
 from website.pages.random_words import random_words
 from website.pages.rust_and_wasm import rust_and_wasm
@@ -19,13 +18,13 @@ with root:
     Directory('business_novels')
     Directory('engineering_management')
     Directory('icons')
+    Directory('make')
     Directory('resume')
 
     aughey()
     bash()
     hashtables()
     macos_packages()
-    make()
     memory_match()
     random_words()
     rust_and_wasm()
