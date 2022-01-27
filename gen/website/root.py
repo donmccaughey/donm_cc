e70044ec691724400c9d783f2479730e11e43d7c
Don McCaughey <don@donm.cc>
2022-01-26 17:32:21 -0800

    Removed defunct "Rust and Wasm" page.

--- gen/website/root.py
+++ gen/website/root.py
@@ -4,7 +4,6 @@ from website.pages.home_page import home_page
 from website.pages.macos_packages import macos_packages
 from website.pages.memory_match import memory_match
 from website.pages.random_words import random_words
-from website.pages.rust_and_wasm import rust_and_wasm
 from website.pages.science_fiction import science_fiction
 
 
@@ -25,5 +24,4 @@ with root:
     macos_packages()
     memory_match()
     random_words()
-    rust_and_wasm()
     science_fiction()
