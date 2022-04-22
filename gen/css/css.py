d205c770c34bad77eb1c97abc457b9eadba4bff1
Don McCaughey <don@donm.cc>
2022-04-21 22:38:54 -0700

    Control the sort order of CSS rules merged into pages.
    When creating a `CSS` object, wrap each top-level `tinycss2` node in
    either a `Rule`, `MediaQuery` or `UnrecognizedRule` class.  These
    classes define a `name` attribute and `serialize()` and `sort_key()`
    methods.
    
    Add static methods `is_media_query()` and `is_rule()` to `CSS` to test
    nodes before wrapping them.
    
    The `Rule` class additionally has methods `is_class_rule()`,
    `is_element_rule()` and `is_id_rule()` for subcategorizing.  The
    constants `UNRECOGNIZED_RULE`, `ELEMENT_RULE`, `CLASS_RULE`, `ID_RULE`
    and `MEDIA_QUERY` define the first-level groupings in the sort order;
    the objects `name` attribute defines the second-level grouping.
    
    The `components_name()` helper function builds the canonical name of a
    rule from the `tinycss2` parsed node.

--- gen/css/css.py
+++ gen/css/css.py
@@ -1,6 +1,14 @@
-from typing import Optional
+from typing import Optional, List, Tuple
 
 from tinycss2 import parse_stylesheet
+from tinycss2.ast import IdentToken, LiteralToken, AtRule, ParenthesesBlock, WhitespaceToken, HashToken, QualifiedRule
+
+
+UNRECOGNIZED_RULE = 0
+ELEMENT_RULE = 1
+CLASS_RULE = 2
+ID_RULE = 3
+MEDIA_QUERY = 4
 
 
 class CSS:
@@ -9,11 +17,23 @@ class CSS:
             self.path = path
             with open(path, 'r') as f:
                 self.source = f.read()
-            self.rules = parse_stylesheet(
+            nodes = parse_stylesheet(
                 self.source, skip_comments=True, skip_whitespace=True
             )
+            self.rules = []
+            for node in nodes:
+                if CSS.is_rule(node):
+                    self.rules.append(Rule(node))
+                elif CSS.is_media_query(node):
+                    self.rules.append(MediaQuery(node))
+                else:
+                    self.rules.append(UnrecognizedRule(node))
+        elif rules:
+            self.rules = list(rules)
         else:
-            self.rules = rules if rules else []
+            self.rules = []
+        self.rules.sort(key=lambda r: r.sort_key())
+
 
     def __repr__(self):
         if self.path:
@@ -28,3 +48,107 @@ class CSS:
     def __add__(self, other):
         rules = self.rules + other.rules
         return CSS(rules=rules)
+
+    @staticmethod
+    def is_media_query(node) -> bool:
+        return isinstance(node, AtRule) and node.at_keyword == 'media'
+
+    @staticmethod
+    def is_rule(node) -> bool:
+        return isinstance(node, QualifiedRule)
+
+
+class Rule:
+    def __init__(self, node):
+        self.node = node
+        self.name = components_name(self.node.prelude)
+
+    def __str__(self) -> str:
+        return self.serialize()
+
+    def is_class_rule(self) -> bool:
+        return (
+            isinstance(self.node, QualifiedRule)
+            and isinstance(self.node.prelude[0], LiteralToken)
+            and self.node.prelude[0].value == '.'
+        )
+
+    def is_element_rule(self) -> bool:
+        return (
+            isinstance(self.node, QualifiedRule)
+            and isinstance(self.node.prelude[0], IdentToken)
+            and self.node.prelude[0].value[0].isalpha()
+        )
+
+    def is_id_rule(self) -> bool:
+        return (
+            isinstance(self.node, QualifiedRule)
+            and isinstance(self.node.prelude[0], HashToken)
+        )
+
+    def serialize(self) -> str:
+        return self.node.serialize()
+
+    def sort_key(self) -> Tuple[int, str]:
+        if self.is_element_rule():
+            return ELEMENT_RULE, self.name
+        if self.is_id_rule():
+            return ID_RULE, self.name
+        if self.is_class_rule():
+            return CLASS_RULE, self.name
+        return 0, self.name
+
+
+class MediaQuery:
+    def __init__(self, node):
+        self.node = node
+        self.name = components_name(node.prelude)
+
+    def __str__(self) -> str:
+        return self.serialize()
+
+    def serialize(self) -> str:
+        return self.node.serialize()
+
+    def sort_key(self) -> Tuple[int, str]:
+        return MEDIA_QUERY, self.name
+
+
+class UnrecognizedRule:
+    def __init__(self, node):
+        self.node = node
+        self.name = repr(node)
+
+    def __str__(self) -> str:
+        return self.serialize()
+
+    def serialize(self) -> str:
+        return self.node.serialize()
+
+    def sort_key(self) -> Tuple[int, str]:
+        return UNRECOGNIZED_RULE, self.name
+
+
+def components_name(components) -> str:
+    while isinstance(components[0], WhitespaceToken):
+        del components[0]
+    while isinstance(components[-1], WhitespaceToken):
+        del components[-1]
+
+    name = ''
+    for component in components:
+        match component:
+            case HashToken():
+                name += '#'
+                name += component.value
+            case IdentToken():
+                name += component.lower_value
+            case LiteralToken():
+                name += component.value
+            case ParenthesesBlock():
+                name += components_name(component.content)
+            case WhitespaceToken():
+                name += ' '
+            case _:
+                print(f'Unrecognized component {component}')
+    return name
