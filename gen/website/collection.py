e52a8b1f131d8b6661f9ec7e610fce7be7c30c89
Don McCaughey <don@donm.cc>
2023-01-24 21:44:53 -0800

    Open external links in new tabs.
    Change the `item()` function to add `target=_blank` and `rel=noreferrer`
    to items in link collections (currently just the home page).

--- gen/website/collection.py
+++ gen/website/collection.py
@@ -1,6 +1,7 @@
+from urllib.parse import urlsplit
 from typing import Optional
 
-from markup import Li, A, Img, Strong, Br, Em, Text
+from markup import Li, A, Strong, Em, Text
 
 
 def item(
@@ -9,7 +10,11 @@ def item(
         subtitle: Optional[str] = None,
 ):
     with Li(class_names=['item']):
-        with A(href=href):
+        with A(href=href) as a:
+            scheme, netloc, *_ = urlsplit(href)
+            if scheme or netloc:
+                a.attributes['rel'] = 'noreferrer'
+                a.attributes['target'] = '_blank'
             Strong(title)
             if subtitle:
                 Text(': ')
